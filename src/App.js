cf5f82c3c3d595de43ebc5f1c1c45d15b21a4b79
Aleeza <108815867+AleezaAdnan@users.noreply.github.com>
2024-10-20 11:45:21 +0500

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar'; // Import the Navbar
+import Home from './components/Home';
+import AboutUs from './components/AboutUs';
+import Events from './components/Events';
+import WIEChapter from './components/WIEChapter';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/wie-chapter" element={<WIEChapter />} />
+      </Routes>
+      </>
+    </Router>
   );
 }
 
