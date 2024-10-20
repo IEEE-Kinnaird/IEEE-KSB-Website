ffc969350f97cf8d04b94391c3f2ddb437dfdf75
Aleeza <108815867+AleezaAdnan@users.noreply.github.com>
2024-10-20 12:01:32 +0500

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar'; // Import the Navbar
+import Navbar from './components/Navbar';
 import Home from './components/Home';
 import AboutUs from './components/AboutUs';
 import Events from './components/Events';
-import WIEChapter from './components/WIEChapter';
+import WIEChapter from './components/WieChapter';
 
 function App() {
   return (
