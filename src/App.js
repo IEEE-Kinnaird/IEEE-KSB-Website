74c6b751fd5c88b8cfd9c4b4e243802f862f0f65
rahemeen1 <rahemeenkamran1@gmail.com>
2024-11-05 08:53:17 -0800

    app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import AboutUs from './components/AboutUs';
-import Events from './components/Events';
-import WieChapter from './components/WieChapter';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import AboutUs from "./components/AboutUs";
+import Events from "./components/Events";
+import WieChapter from "./components/WieChapter";
+import Contact from "./components/Contact";
+import Gallery from "./components/Gallery";
 
 function App() {
-  console.log('App is rendering'); // Debugging log
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/wie-chapter" element={<WieChapter />} />
-      </Routes>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/wie-chapter" element={<WieChapter />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
+
 export default App;
