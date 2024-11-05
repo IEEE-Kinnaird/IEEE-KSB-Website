8b21c1cc92d4db01110310df001032866e312da3
rahemeen1 <rahemeenkamran1@gmail.com>
2024-11-05 08:39:05 -0800

    new

---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hero.text">
+        <h1>IEEE</h1>
+        <h3>Kinnaird Student Branch</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
