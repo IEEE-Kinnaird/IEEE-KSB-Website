05146b4293af16f46dba7cf28c78253a09d31111
rahemeen1 <rahemeenkamran1@gmail.com>
2024-11-05 08:45:58 -0800

    footerrecovery

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./footer.css"; // Import the CSS file
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-content">
+          <p> 2024 IEEE KSB. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
